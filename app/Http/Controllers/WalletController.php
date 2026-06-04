8a5e560f2bf247efba2fc2d8ab72ae374f05423a
Mohammed-tareq <mohtareq1999m@gmail.com>
2026-06-04 18:31:52 +0300

    initialize project structure with basic files and configurations

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -8,10 +8,7 @@ use App\Services\CreateWebhookService;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Str;
-use Spatie\WebhookServer\WebhookCall;
-use function Sodium\increment;
 
 
 class WalletController extends Controller
